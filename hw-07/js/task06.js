61aaa10512e8a14a1095f8d24069198f487789e8
Sergey Zelinsky <zelinsky.serega@gmail.com>
2019-12-17 11:01:09 +0200

    Fixed task06

--- hw-07/js/task06.js
+++ hw-07/js/task06.js
@@ -1,11 +1,13 @@
 const inputText = document.getElementById('validation-input');
+const inputSybols = Number(inputText.dataset.length);
 
 const checkLength = () => {
 	console.log(inputText.value.length);
-	if (inputText.value.length === 6) {
+	if (inputText.value.length === inputSybols) {
 		inputText.classList.add("valid");	
 	} else {
 		inputText.classList.add("invalid");	
 	}
 }
 inputText.addEventListener('blur', checkLength);
+console.log(inputSybols);
